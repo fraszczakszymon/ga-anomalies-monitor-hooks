12f7a9bcafe205cd94d0e41e0b6afbbfe0072f2f
Szymon Fraszczak <fraszczak.szymon+github@gmail.com>
2016-01-31 10:33:12 +0100

    Add license

---
+++ cli.js
@@ -0,0 +1,8 @@
+/**
+ * GA Anomalies Monitor h ooks(https://github.com/fraszczakszymon/ga-anomalies-monitor-hooks)
+ *
+ * Copyright © 2016 Frąszczak Szymon. All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE.md file in the root directory of this source tree.
+ */
