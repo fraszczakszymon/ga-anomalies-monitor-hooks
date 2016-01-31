590ec1c208115b34816953139ef5ce6e02f9a17f
Szymon Fraszczak <fraszczak.szymon+github@gmail.com>
2016-01-31 13:53:01 +0100

    Slack integration

--- modules/hooks/slack/index.js
+++ modules/hooks/slack/index.js
@@ -7,9 +7,47 @@
  * LICENSE.md file in the root directory of this source tree.
  */
 
-/*global module*/
+/*global module, require*/
+var config = require('config'),
+	moment = require('moment-timezone'),
+	slack = require('slack/slack');
+
+function sendQueryMessage(build, query, data) {
+	var date = moment(data.date).format('MMMM DD, HH:mm'),
+		url = config.client.url + '/build/' + build.id + '/query/' + query.id,
+		title = 'Detected ' + (data.change > 0 ? 'growth' : 'drop')+ ' in *' + query.title + '*',
+		text = '<' + url +'|Build #' + build.id + ': ' + '*' + query.title + '*>, ' + date;
+
+	slack.send(title, [
+		{
+			fallback: 'Build #' + build.id + ': ' + query.title + ', ' + date,
+			text: text,
+			fields: [
+				{
+					title: 'Change',
+					value: (data.change > 0 ? '+' : '') + data.change.toFixed(2) + "%",
+					short: true
+				},
+				{
+					title: 'Value',
+					value: data.value,
+					short: true
+				}
+			],
+			mrkdwn_in: ['text'],
+			color: (data.change > 0 ? '#689F38' : '#E53935')
+		}
+	]);
+}
+
 function onSuccessBuild(build) {
 	console.log('Success:', build.id);
+	build.queries.forEach((query) => {
+		var data = query.data[query.data.length - 1];
+		if (data.exceeded) {
+			sendQueryMessage(build, query, data);
+		}
+	});
 }
 
 function dispatch(event, data) {
--- modules/slack/slack.js
+++ modules/slack/slack.js
@@ -7,7 +7,30 @@
  * LICENSE.md file in the root directory of this source tree.
  */
 
+/*global module, require*/
+var config = require('config'),
+	fetch = require('node-fetch'),
+	FormData = require('form-data');
+
+function sendMessage(text, attachments) {
+	var data = {
+			attachments: attachments,
+			channel: config.hooks.slack.channel,
+			icon_emoji: config.hooks.slack.icon_emoji,
+			text: text,
+			username: config.hooks.slack.username
+		},
+		form = new FormData(),
+		url = 'https://hooks.slack.com/services/' + config.hooks.slack.token;
+
+	form.append('payload', JSON.stringify(data))
+	fetch(url, {
+		method: 'POST',
+		body: form
+	}).then((res) => {return res.text()})
+		.then((body) => {console.log(body)});
+}
 
 module.exports = {
-	test :2
-}
+	send: sendMessage
+};
